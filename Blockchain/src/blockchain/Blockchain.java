10d0a78d6352f9443bcf6492ca1a8a45ccd6adeb
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-09-11 14:11:46 -0500

    Digital Signature [Not fully implemented yet]

--- Blockchain/src/blockchain/Blockchain.java
+++ Blockchain/src/blockchain/Blockchain.java
@@ -5,9 +5,18 @@
  */
 package blockchain;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONObject;
 
 /**
  *
@@ -18,40 +27,59 @@ public class Blockchain {
     public static final int DIFFICULTY = 3;
     
     public static HashMap<String, Object> javaContractsMap = new HashMap<String, Object>(){{
-        put("JavaCoinContract", new JavaCoinContract() );
+        put("JavaCatContract", new JavaCatContract("addrx1",50.0) );
     }};
-    
-    
     
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // Chain creation
-        Chain theChain = new Chain();
-
-        //Minner creation
-        Miner miner1 = new Miner(new TransactionPool());
-        
-        //Adding Transaction to the miner's transacion pool
-        miner1.txPool.addTransaction(new Transaction("addrx1","JavaCoinContract",new String[]{"Destiny", "10.0"}));
-        miner1.txPool.addTransaction(new Transaction("addrx2","JavaCoinContract",new String[]{"Destiny", "20.0"}));
-        miner1.txPool.addTransaction(new Transaction("addrx3","JavaCoinContract",new String[]{"Destiny", "30.0"}));
-        
-        //Create Genesis Block 
-        Bloque b = new Bloque(0, LocalDateTime.now(), new ArrayList(), "0");
-        theChain.addBlock(b);
-
-          
-        //Miner Generates Candidate Block
-        Bloque minerBlock = miner1.GenerateCandiateBock();
-        miner1.broadCastBlock(minerBlock);
-        
-        theChain.addBlock(minerBlock);
-
-        
-        theChain.listAllBlocks();
+    public static void main(String[] args) throws Exception {
+        try {
+            DigitalSignature digiSig = new DigitalSignature();
+            JSONObject obj = digiSig.sender();
+            boolean result = digiSig.receiver(obj);
+            System.out.println(result);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(DigitalSignature.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidAlgorithmParameterException ex) {
+            Logger.getLogger(DigitalSignature.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeyException ex) {
+            Logger.getLogger(DigitalSignature.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(DigitalSignature.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SignatureException ex) {
+            Logger.getLogger(DigitalSignature.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidKeySpecException ex) {
+            Logger.getLogger(DigitalSignature.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//        // Chain creation
+//        Chain theChain = new Chain();
+//
+//        //Minner creation
+//        Miner miner1 = new Miner(new TransactionPool());
+//        
+//        //Adding Transaction to the miner's transacion pool
+//        //check transaction strucutre before adding to txpool
+//        // JAVA SGIN 
+//        miner1.txPool.addTransaction(new Transaction("addrx1","JavaCatContract",new String[]{"addrx2", "1.0"}));
+//        miner1.txPool.addTransaction(new Transaction("addrx2","JavaCatContract",new String[]{"Destiny", "5.0"}));
+//        miner1.txPool.addTransaction(new Transaction("addrx3","JavaCatContract",new String[]{"Destiny", "30.0"}));
+//        
+//        //Create Genesis Block 
+//        Bloque b = new Bloque(0, LocalDateTime.now(), new ArrayList(), "0");
+//        theChain.addBlock(b);
+//
+//          
+//        //Miner Generates Candidate Block
+//        Bloque minerBlock = miner1.GenerateCandiateBock();
+//        miner1.broadCastBlock(minerBlock);
+//        
+//        theChain.addBlock(minerBlock);
+//        
+//        theChain.listAllBlocks();
+//        
+//        System.out.println(JavaCatContract.balances);
         
         
     }
--- Blockchain/src/blockchain/JavaCatContract.java
+++ Blockchain/src/blockchain/JavaCatContract.java
@@ -12,17 +12,22 @@ import java.util.HashMap;
  *
  * @author laptop
  */
-public class JavaCoinContract extends JavaContract{
+public class JavaCatContract extends JavaContract{
     
-    public static HashMap<String, Double> balances;
-    public JavaCoinContract() {
+    public static HashMap<String, Double> balances; 
+    public String owner;
+    
+    public JavaCatContract(String owner, Double capital) {
         this.balances = new HashMap<String, Double>();
+        this.owner = owner;
+        balances.put(owner,capital);
     }
 
     @Override
     public boolean ejecutar(Transaction tx) {
         try{
             if(balances.get(tx.from_address) < Double.parseDouble(tx.data[1])) return false;
+            if(balances.get(tx.data[0]) == null) balances.put(tx.data[0],0.0);
             balances.put(tx.from_address, balances.get(tx.from_address) - Double.parseDouble(tx.data[1]));
             balances.put(tx.data[0], balances.get(tx.data[0]) + Double.parseDouble(tx.data[1]));
             return true;
